3f060b2ecab50eb66669c7b26459a18414eb1451
Alex Cannan <alexfcannan@gmail.com>
2022-11-27 22:20:43 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lissamp',
+    author='Alex Cannan',
+    author_email='alexfcannan@gmail.com',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
